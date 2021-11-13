1fc8e798d7ca5c5f1a7530e70c7d83e5d81b24fc
Sergey <sniper411@mail.ru>
2021-11-13 21:12:25 +0300

    added English alfavit

---
+++ 12/Program.cs
@@ -0,0 +1,10 @@
+﻿//12. Удалить вторую цифру трёхзначного числа
+int n = 790;
+int delTwo(int n)
+{
+    int num1 = n / 100;
+    int num3 = n % 10;
+    return num1 * 10 + num3;
+
+}
+Console.WriteLine(delTwo(n));
---
+++ ABC/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вывести на экран английский алфавит
+// используем символьную арифметику + цикл
+
+char glif = 'a';
+//glif = (char) (glif + 1);
+
+//1 способ
+while (glif<=122)  //('z')
+{
+    System.Console.WriteLine(glif);
+    // gliff = (char) (gliff + 1);
+    glif++;
+}
+
+
+// 2 способ
+for (glif = 'a'; glif <= 300; glif++)
+{
+    System.Console.Write(glif + " ");
+}
