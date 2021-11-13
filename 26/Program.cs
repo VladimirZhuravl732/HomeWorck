a011ae9b777a0a2cc809761894a1345c890d2c7a
Sergey <sniper411@mail.ru>
2021-11-13 21:33:03 +0300

    EXAMPLE 26

---
+++ 26/Program.cs
@@ -0,0 +1,38 @@
+﻿// Возведите число А в натуральную степень числа В используя цикл
+int toPower(int number, int degree) //2, 4
+{
+    int result = 1;  // result =1
+
+    for (int i = 1; i <= degree; i++)
+    {
+        result = result * number;  // result =2
+    }
+    return result;
+}
+
+if (8 == toPower(2, 3))
+{
+    System.Console.WriteLine("Работает корректно");
+}
+else
+{
+    System.Console.WriteLine("Ошибочка вышла");
+}
+
+if (225 == toPower(15, 2))
+{
+    System.Console.WriteLine("Работает корректно");
+}
+else
+{
+    System.Console.WriteLine("Ошибочка вышла");
+}
+
+if (1024 == toPower(2, 10))
+{
+    System.Console.WriteLine("Работает корректно");
+}
+else
+{
+    System.Console.WriteLine("Ошибочка вышла");
+}
