28085ddde38aed5274441b265d1e62611cb1a550
Sergey <sniper411@mail.ru>
2021-11-15 14:04:59 +0300

    added ABC 23

--- 23/Program.cs
+++ 23/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Показать таблицу квадратов чисел от 1 до N
+
+int number = 1;
+int N = 10;
+
+while (number < N)
+{
+    int Square = number * number;
+    System.Console.Write(number + " " + "квадрат числа");
+    System.Console.WriteLine(" " + Square);
+    number++;
+}
